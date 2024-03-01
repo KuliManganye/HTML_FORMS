669c651e37c4708c7504bfd118fd17cda89ba081
KuliManganye <94355261+KuliManganye@users.noreply.github.com>
2024-03-02 00:56:28 +0200

    Add files via upload

---
+++ README.md
@@ -0,0 +1,5 @@
+# CONTACT FORM
+## This is a contact form using simple HTML Forms
+### It gives the user fields to fill in then sends an email to the owner of the form as to the question that the user is asking
+
+![img.png](img.png)
---
+++ main.py
@@ -0,0 +1,33 @@
+from flask import Flask, request, render_template
+from dotenv import load_dotenv
+import os
+import smtplib
+
+
+load_dotenv()
+
+EMAIL = os.getenv("email")
+PASSWORD = os.getenv("PASSWORD")
+
+app = Flask(__name__)
+
+
+@app.route("/", methods=["GET", "POST"])
+def contact():
+    if request.method == "POST":
+        data = request.form
+        send_email(data["name"], data["email"], data["phone"], data["message"])
+        return render_template("contact.html", msg_sent=True)
+    return render_template("contact.html", msg_sent=False)
+
+
+def send_email(name, email, phone, message):
+    email_message = f"Subject:New Message\n\nName: {name}\nEmail: {email}\nPhone: {phone}\nMessage: {message}"
+    with smtplib.SMTP("smtp.gmail.com") as connection:
+        connection.starttls()
+        connection.login(EMAIL, PASSWORD)
+        connection.sendmail(EMAIL, EMAIL, email_message)
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
